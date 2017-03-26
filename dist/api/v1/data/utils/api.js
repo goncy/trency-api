d10f54d0f4da9d91fa6ff9571e7636348a6186c8
goncy <goncy@live.com.ar>
2017-03-25 21:59:43 -0300

    redeploy

--- dist/api/v1/data/utils/api.js
+++ dist/api/v1/data/utils/api.js
@@ -17,13 +17,15 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
+var r = _requestPromise2.default.defaults({ 'proxy': 'http://111.7.174.141:8000' });
+
 var apiRequest = exports.apiRequest = function () {
   var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(branch, tipo, key) {
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            return _context.abrupt('return', (0, _requestPromise2.default)({
+            return _context.abrupt('return', r({
               url: tipo === 'arribos' ? _constants.API_URLS.arrivals : _constants.API_URLS.positions,
               headers: _constants.API_HEADERS,
               qs: {
