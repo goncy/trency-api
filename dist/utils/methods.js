d2295cfac7abcac7c99770d2828805d646020c20
goncy <gonzalo.pozzo4@gmail.com>
2017-09-27 15:11:16 -0300

    redeploy

--- dist/utils/methods.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var randomString = exports.randomString = function randomString() {
-  return Math.random().toString(36).substring(5, 15);
-};
